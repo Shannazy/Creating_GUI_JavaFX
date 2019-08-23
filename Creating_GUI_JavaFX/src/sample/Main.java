339c681f6b830abb6e5b1942319db1211468a116
Shannazy <shaziislam417@gmail.com>
2019-08-23 17:48:32 -0400

    Created the window with a click me button

--- Creating_GUI_JavaFX/src/sample/Main.java
+++ Creating_GUI_JavaFX/src/sample/Main.java
@@ -1,18 +1,27 @@
 package sample;
 
-import javafx.*
+import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    Button button;
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("First GUI");   // Stage is the window and the title is First Gui
+        button = new Button();  //Create new button object
+        button.setText("Click Me!");    //Label for the button
+
+        StackPane layout = new StackPane(); //This is the general simple layout
+        layout.getChildren().add(button);
+
+        Scene page = new Scene(layout, 500, 500);
+        primaryStage.setScene(page);
         primaryStage.show();
     }
 
